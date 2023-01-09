d10b93490accfe57bd9a365e551ad74882d8e0ba
David Fang <fanghuateng0621@gmail.com>
2023-01-09 16:35:05 +0800

    change model

--- app/Mail/DriverEmailVerificationByAdmin.php
+++ app/Mail/DriverEmailVerificationByAdmin.php
@@ -2,7 +2,7 @@
 
 namespace App\Mail;
 
-use App\User;
+use App\Driver;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
@@ -19,7 +19,7 @@ class DriverEmailVerificationByAdmin extends Mailable
      *
      * @return void
      */
-    public function __construct(User $user)
+    public function __construct(Driver $user)
     {
         $this->user = $user;
     }
