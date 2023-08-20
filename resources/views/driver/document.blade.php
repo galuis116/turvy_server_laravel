c84cb175e1bc50a1770432dca5a24268e84ca18a
Sheldon Thompson <sheldoontho@gmail.com>
2023-08-21 00:31:16 +0200

    document image centered in driver page and style updated in admin driver document subpage

--- resources/views/driver/document.blade.php
+++ resources/views/driver/document.blade.php
@@ -3,11 +3,11 @@
 
 
 @section('style')
+    <link href="{{ asset('driver-panel/plugins/bootstrap-datepicker/bootstrap-datepicker.min.css') }}" rel="stylesheet"
+        type="text/css" />
 
-    <link href="{{asset('driver-panel/plugins/bootstrap-datepicker/bootstrap-datepicker.min.css')}}" rel="stylesheet" type="text/css" />
-
-    <link rel="stylesheet" type="text/css" href="{{asset('driver-panel/plugins/bootstrap-fileinput/bootstrap-fileinput.css')}}"/>
-
+    <link rel="stylesheet" type="text/css"
+        href="{{ asset('driver-panel/plugins/bootstrap-fileinput/bootstrap-fileinput.css') }}" />
 @endsection
 
 
@@ -19,42 +19,43 @@
     <div class="col-md-12">
 
 
-            @csrf
+        @csrf
 
-            <div class="form-group">
-                <div class="col-md-12">
-                    @include('partials.message')
-                </div>
+        <div class="form-group">
+            <div class="col-md-12">
+                @include('partials.message')
             </div>
+        </div>
 
-            <div class="form-group">
+        <div class="form-group">
 
             @forelse($documents as $one)
+                <div class="col-md-12">
 
-            <div class="col-md-12">
+                    <a href="{{ route('driver.document.edit', ['id' => $one['document_id']]) }}" class="card"
+                        style="padding: 5px; margin-bottom: 10px;border: none; {{ isset($one['document_status']) && $one['document_status'] == 1 ? 'background-color: #e4d1d1;' : '' }}">
 
-                <a href="{{ route('driver.document.edit',['id' => $one['document_id']]) }}" class="card" style="padding: 5px; margin-bottom: 10px;border: none;">
+                        <div class="card-body" style="justify-content:center;align-items:center;display:flex">
 
-                    <div class="card-body" style="justify-content:center;align-items:center;display:flex">
-
-                        <div class="col-md-11">
-                            <div  style="padding: 10px; text-align: left;">Completed</div>
-                            <div style="padding: 10px;text-align: left;font-weight: bold">{{$one['document_name']}}</div>
-                        </div>
-                        <div class="col-md-1">
-                            @if(isset($one['document_status']))
-                                @if($one['document_status']==1)
-                                    <div class="badge bg-green">Approved</div>
-                                @else
-                                    <div class="badge bg-red">Pending</div>
+                            <div class="col-md-12">
+                                <div style="padding: 10px; text-align: left;">Completed</div>
+                                <div style="padding: 10px;text-align: left;font-weight: bold">{{ $one['document_name'] }}
+                                </div>
+                            </div>
+                            <div class="col-md-2">
+                                @if (isset($one['document_status']))
+                                    @if ($one['document_status'] == 1)
+                                        <div class="badge bg-green" style="background-color:green">Approved</div>
+                                    @else
+                                        <div class="badge bg-red" style="background-color:red">Pending</div>
+                                    @endif
                                 @endif
-                            @endif
+                            </div>
+
                         </div>
 
-                    </div>
-
-                </a>
-            </div>
+                    </a>
+                </div>
 
             @empty
 
@@ -63,10 +64,9 @@
                     <p>Documents required by drivers are not created in this city. Please contact your supervisor.</p>
 
                 </div>
-
             @endforelse
 
-            </div>
+        </div>
 
 
 
@@ -79,22 +79,18 @@
 
 
 @section('script')
+    <script src="{{ asset('driver-panel/plugins/bootstrap-datepicker/bootstrap-datepicker.min.js') }}"></script>
 
-    <script src="{{asset('driver-panel/plugins/bootstrap-datepicker/bootstrap-datepicker.min.js')}}"></script>
-
-    <script type="text/javascript" src="{{asset('driver-panel/plugins/bootstrap-fileinput/bootstrap-fileinput.js')}}"></script>
+    <script type="text/javascript" src="{{ asset('driver-panel/plugins/bootstrap-fileinput/bootstrap-fileinput.js') }}">
+    </script>
 
 
 
     <script>
-
         $('.expiredate').datepicker({
 
             autoclose: true
 
         })
-
     </script>
-
 @endsection
-
--- resources/views/driver/document_edit.blade.php
+++ resources/views/driver/document_edit.blade.php
@@ -3,24 +3,22 @@
 
 
 @section('style')
+    <link href="{{ asset('driver-panel/plugins/bootstrap-datepicker/bootstrap-datepicker.min.css') }}" rel="stylesheet"
+        type="text/css" />
 
-    <link href="{{asset('driver-panel/plugins/bootstrap-datepicker/bootstrap-datepicker.min.css')}}" rel="stylesheet" type="text/css" />
-
-    <link rel="stylesheet" type="text/css" href="{{asset('driver-panel/plugins/bootstrap-fileinput/bootstrap-fileinput.css')}}"/>
-
+    <link rel="stylesheet" type="text/css"
+        href="{{ asset('driver-panel/plugins/bootstrap-fileinput/bootstrap-fileinput.css') }}" />
 @endsection
 
 
 
 @section('content')
-
-
-
     <div style="display:flex; justify-content:center; align-items: center;">
 
 
 
-        <form method="POST" class="form-horizontal" action="{{route('driver.document.update', ['id' => $document['document_id']])}}" enctype="multipart/form-data">
+        <form method="POST" class="form-horizontal"
+            action="{{ route('driver.document.update', ['id' => $document['document_id']]) }}" enctype="multipart/form-data">
 
             @csrf
 
@@ -35,66 +33,73 @@
 
                 <div class="card" style="padding: 20px; margin-bottom: 15px;">
 
-                    <div class="card-header" style="padding: 10px;text-align:center;font-weight:bold;margin-bottom:15px">{{$document['document_name']}}</div>
-                    <div class="block-header col-lg-12 col-md-8 col-sm-12 col-xs-12" style="background-color: #003C77;margin-bottom: 15px;">
-                        <a href="{{route('driver.document')}}" class="btn bg-grey waves-effect pull-left" style="padding-left: 5px;display: flex; align-items: center; color: white;"><i class="material-icons">keyboard_backspace</i><span>Turvy</span></a>
+                    <div class="card-header" style="padding: 10px;text-align:center;font-weight:bold;margin-bottom:15px">
+                        {{ $document['document_name'] }}</div>
+                    <div class="block-header col-lg-12 col-md-8 col-sm-12 col-xs-12"
+                        style="background-color: #003C77;margin-bottom: 15px;">
+                        <a href="{{ route('driver.document') }}" class="btn bg-grey waves-effect pull-left"
+                            style="padding-left: 5px;display: flex; align-items: center; color: white;"><i
+                                class="material-icons">keyboard_backspace</i><span>Turvy</span></a>
                     </div>
-                    <p style="padding: 10px; font-size:20px;font-weight:bold; width:550px; margin_top:10px ">{{$document['document_title']}}</p>
-                    <p style="padding: 10px; font-size:16px; width:550px">{{$document['document_description']}}</p>
-                    <p style="padding: 10px; font-size:17px; display:flex; align-items: center; color:blue"><i class="material-icons">info</i><span>What is this?</span></p>
-                    <img  src="{{asset($document['document_imageurl'])}}" width="550px" height="550px" alt="Document Reference Image"/>
-                    <div class="card-body">
-
-                        <div class="fileinput fileinput-new" data-provides="fileinput" >
+                    <p style="padding: 10px; font-size:20px;font-weight:bold; width:550px; margin_top:10px ">
+                        {{ $document['document_title'] }}</p>
+                    <p style="padding: 10px; font-size:16px; width:550px">{{ $document['document_description'] }}</p>
+                    @if ($document['document_imageurl'] != '')
+                        <p style="padding: 10px; font-size:17px; display:flex; align-items: center; color:blue"><i
+                                class="material-icons">info</i><span>What is this?</span></p>
+                        <img src="{{ asset($document['document_imageurl']) }}" width="550px" height="550px"
+                            alt="Document Reference Image" />
+                    @endif
+                    <div class="card-body"
+                        style="align-items: center;
+                            justify-content: center;
+                            display: flex;">
+                        <div class="fileinput fileinput-new" data-provides="fileinput">
 
                             <div class="fileinput-new thumbnail" style="width:200px;height:200px;">
 
-                                @if($document['document_url'] != '')
-                                    @if($document['document_status']==1)
-                                        <div class="badge bg-green">Approved</div>
-                                    @else
-                                        <div class="badge bg-red">Pending</div>
-                                    @endif
-                                    <a href="{{asset($document['document_url'])}}" target="_blank">
+                                @if ($document['document_url'] != '')
+                                    <a href="{{ asset($document['document_url']) }}" target="_blank">
 
-                                        <img onerror="this.src='{{asset('images/download.jfif')}}'" src="{{asset($document['document_url'])}}" width="190px" height="116px" alt=""/>
+                                        <img onerror="this.src='{{ asset('images/download.jfif') }}'"
+                                            src="{{ asset($document['document_url']) }}" width="190px" height="116px"
+                                            alt="" />
 
                                     </a>
-                                    
-
                                 @else
-
                                     <a href="#">
 
-                                        <img src="{{asset('images/no-image.png')}}" width="190px" height="116px" alt=""/>
+                                        <img src="{{ asset('images/no-image.png') }}" width="190px" height="116px"
+                                            alt="" />
 
                                     </a>
-
                                 @endif
 
                             </div>
 
-                            <div class="fileinput-preview fileinput-exists thumbnail" style="width:200px;height:200px;"></div>
+                            <div class="fileinput-preview fileinput-exists thumbnail" style="width:200px;height:200px;">
+                            </div>
 
                             <div>
 
-                                        <span class="btn btn-default btn-file">
+                                <span class="btn btn-default btn-file">
 
-                                            <span class="fileinput-new">Select File </span>
+                                    <span class="fileinput-new">Select File </span>
 
-                                            <span class="fileinput-exists">Change </span>
+                                    <span class="fileinput-exists">Change </span>
 
-                                            <input type="file" name="document_file" >
+                                    <input type="file" name="document_file">
 
-                                        </span>
+                                </span>
 
-                                <a href="#" class="btn btn-danger fileinput-exists" data-dismiss="fileinput">Remove </a>
+                                <a href="#" class="btn btn-danger fileinput-exists" data-dismiss="fileinput">Remove
+                                </a>
 
                             </div>
 
                         </div>
 
-                        <div>
+                        {{-- <div>
 
                             <label class="mb-10">Expire Date</label>
 
@@ -110,7 +115,7 @@
 
                             </div>
 
-                        </div>
+                        </div> --}}
 
                     </div>
 
@@ -123,7 +128,7 @@
             <div class="form-group">
 
 
-                    <button type="submit" class="btn btn-primary form-control">Upload Document</button>
+                <button type="submit" class="btn btn-primary form-control">Upload Document</button>
 
 
             </div>
@@ -135,30 +140,23 @@
 
 
     </div>
-
-
-
 @endsection()
 
 
 
 @section('script')
+    <script src="{{ asset('driver-panel/plugins/bootstrap-datepicker/bootstrap-datepicker.min.js') }}"></script>
 
-    <script src="{{asset('driver-panel/plugins/bootstrap-datepicker/bootstrap-datepicker.min.js')}}"></script>
-
-    <script type="text/javascript" src="{{asset('driver-panel/plugins/bootstrap-fileinput/bootstrap-fileinput.js')}}"></script>
+    <script type="text/javascript" src="{{ asset('driver-panel/plugins/bootstrap-fileinput/bootstrap-fileinput.js') }}">
+    </script>
 
 
 
     <script>
-
         $('.expiredate').datepicker({
 
             autoclose: true
 
         })
-
     </script>
-
 @endsection
-
